8e74e79c7f54b1bd9f050701aea6d17983092c85
Emily Platzer <emilyplatzer@gmail.com>
2014-05-19 16:03:03 -0600

    User can create a new project

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140515162330) do
+ActiveRecord::Schema.define(version: 20140519212502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: true do |t|
+    t.string "project_name"
+    t.string "project_api"
+  end
 
   create_table "users", force: true do |t|
     t.string   "first_name"
