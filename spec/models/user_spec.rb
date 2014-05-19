c34f1d38bd20d49cbde8016c23b36ac8f2664615
Gerard Cote <grcote@gmail.com>
2014-05-19 15:28:46 -0600

    [#71355796] users cannot register without an email
    Signed-off-by: Bebe Peng <bebepeng@gmail.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,12 +3,17 @@ require 'spec_helper'
 describe User do
   describe "Validations" do
     describe "on a new user" do
-
       it "should not be valid with a confirmation mismatch" do
         user = User.new(password: 'alpha', password_confirmation: 'beta')
-        expect(user).to_not be_valid
+        user.save
+        expect(user.errors[:password_confirmation]).to match_array ["doesn't match Password"]
       end
 
+      it "should not be valid without an email" do
+        user = User.new(email: "")
+        user.save
+        expect(user.errors[:email]).to match_array ["can't be blank"]
+      end
     end
   end
 end
