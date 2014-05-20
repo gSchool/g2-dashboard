e0844b29241231a6c3c8e69e6e15a065c67473bd
Emily Platzer <emilyplatzer@gmail.com>
2014-05-20 14:48:50 -0600

    [#71356798] User cant create a project with no project name
    Signed-off-by: Bebe Peng <bebepeng@gmail.com>

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ActiveRecord::Base
+  validates :project_name, :presence => true
   validates :project_name, :uniqueness => { case_sensitive: false }
   validates :project_api, :uniqueness => true
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -2,6 +2,15 @@ require 'spec_helper'
 
 describe Project do
   describe 'Validations' do
+
+    it 'requires project name to not be blank' do
+      new_project = Project.new(
+        :project_name => '',
+        :project_api => SecureRandom.uuid)
+      new_project.valid?
+      expect(new_project.errors[:project_name]).to match_array ["can't be blank"]
+    end
+
     it 'requires a unique project name (case insensitive)' do
       name = 'CoolestProjectEver'
       Project.create!(
