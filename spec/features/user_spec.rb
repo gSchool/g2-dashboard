b4d2dc5ec85510309a30f983887e5e14b12e1c18
Emily Platzer <emilyplatzer@gmail.com>
2014-05-20 10:15:15 -0600

    Refactor support/features_methods to be more descriptive and dry
    Signed-off-by: Jenny Allar <jennyallar@gmail.com>

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,36 +1,32 @@
 require 'spec_helper'
 
 feature 'User functions' do
-  before do
-    create_user
-  end
 
   scenario 'allows users to sign up' do
-    fill_in 'Password Confirmation', with: @password
-    click_on 'Submit'
-
+    register_user
     expect(page).to have_content("Hello, #{@first_name} #{@last_name}")
   end
 
   scenario 'displays an error when user password and password confirmation do not match' do
-    fill_in 'Password Confirmation', with: "nosiree"
+    visit '/'
+    click_on 'Sign Up'
+    fill_in 'First Name', with: @first_name
+    fill_in 'Last Name', with: @last_name
+    fill_in 'Email', with: 'awesomesauce@example.com'
+    fill_in 'Password', with: 'yepperz'
+    fill_in 'Password Confirmation', with: 'noperz'
     click_on 'Submit'
-
     expect(page).to have_content("Password confirmation doesn't match Password")
   end
 
   it 'a newly registered user is automatically logged in' do
-    fill_in 'Password Confirmation', with: @password
-    click_on 'Submit'
-
+    register_user
     expect(page).to have_content("Hello, #{@first_name} #{@last_name}")
     expect(page).to have_no_link("Sign Up")
   end
 
   it 'a user can logout' do
-    fill_in 'Password Confirmation', with: @password
-    click_on 'Submit'
-
+    register_user
     click_on 'Sign Out'
     expect(page).to have_no_content("Hello, #{@first_name} #{@last_name}")
     expect(page).to have_link("Sign Up")
--- spec/support/features_methods.rb
+++ spec/support/features_methods.rb
@@ -1,13 +1,13 @@
-def create_user
+def register_user
   @first_name = 'Arya'
   @last_name = 'Stark'
-  @email = 'awesomesauce@example.com'
-  @password = 'yepperz'
 
   visit '/'
   click_on 'Sign Up'
   fill_in 'First Name', with: @first_name
   fill_in 'Last Name', with: @last_name
-  fill_in 'Email', with: @email
-  fill_in 'Password', with: @password
+  fill_in 'Email', with: 'awesomesauce@example.com'
+  fill_in 'Password', with: 'yepperz'
+  fill_in 'Password Confirmation', with: 'yepperz'
+  click_on 'Submit'
 end
