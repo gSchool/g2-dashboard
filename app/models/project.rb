5f6cb3a09134512400bbb1b469a788d140967b41
Emily Platzer <emilyplatzer@gmail.com>
2014-05-20 11:52:56 -0600

    [#71356798] Validations for project name are now case insensitive
    Signed-off-by: Bebe Peng <bebepeng@gmail.com>

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,4 @@
 class Project < ActiveRecord::Base
-  validates :project_name, :uniqueness => true
+  validates :project_name, :uniqueness => { case_sensitive: false }
   validates :project_api, :uniqueness => true
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -4,15 +4,30 @@ describe Project do
   describe 'Validations' do
     it 'requires a unique project name (case insensitive)' do
       name = 'CoolestProjectEver'
-      Project.create!(:project_name => name, :project_api => SecureRandom.uuid)
-      new_project = Project.new(:project_name => name, :project_api => SecureRandom.uuid)
+      Project.create!(
+        :project_name => name,
+        :project_api => SecureRandom.uuid)
+      new_project = Project.new(
+        :project_name => name,
+        :project_api => SecureRandom.uuid)
       new_project.valid?
       expect(new_project.errors[:project_name]).to match_array ["has already been taken"]
+
+      other_new_project = Project.new(
+        :project_name => name.downcase,
+        :project_api => SecureRandom.uuid)
+      other_new_project.valid?
+      expect(other_new_project.errors[:project_name]).to match_array ["has already been taken"]
     end
+
     it 'requires a unique project api' do
       uuid = SecureRandom.uuid
-      Project.create!(:project_name => 'CoolestProjectEver', :project_api => uuid)
-      new_project = Project.new(:project_name => 'CoolestProjectEver', :project_api => uuid)
+      Project.create!(
+        :project_name => 'CoolestProjectEver',
+        :project_api => uuid)
+      new_project = Project.new(
+        :project_name => 'CoolestProjectEver',
+        :project_api => uuid)
       new_project.valid?
       expect(new_project.errors[:project_api]).to match_array ["has already been taken"]
     end
