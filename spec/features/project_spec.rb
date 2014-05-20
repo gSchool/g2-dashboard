4d3033ad62c69b0a144b6a908992310b8cc2f1c8
Emily Platzer <emilyplatzer@gmail.com>
2014-05-20 15:21:00 -0600

    added helper method create_project
    also wrapped support/featuresmethods in a module
    
    Signed-off-by: Bebe Peng <bebepeng@gmail.com>

--- spec/features/project_spec.rb
+++ spec/features/project_spec.rb
@@ -1,32 +1,16 @@
 require 'spec_helper'
 
 feature 'Project functions' do
-
-  before do
-    register_user
-  end
-
   scenario 'allows a user to create a project' do
-    visit '/projects'
-
-    click_on 'Add New Project'
-    fill_in 'Name of Project', with: 'Murder Joffrey'
-    click_on 'Add Project'
+    create_project
 
     expect(page).to have_content('Murder Joffrey')
   end
 
   scenario 'User can see error for incorrect Project creation' do
-    visit '/projects'
-    project_name = 'Murder Joffrey'
-    click_on 'Add New Project'
-    fill_in 'Name of Project', with: project_name
-    click_on 'Add Project'
+    create_project
 
-    click_on 'Add New Project'
-    fill_in 'Name of Project', with: project_name
-    click_on 'Add Project'
+    create_project
     expect(page).to have_content('Project name has already been taken')
   end
-
 end
