cefeb181a06acc3b5c2c42bfb0f2c97df967f3aa
Emily Platzer <emilyplatzer@gmail.com>
2014-05-20 10:36:47 -0600

    Updated user model spec with .valid? instead of .save
    Signed-off-by: Bebe Peng <bebepeng@gmail.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,26 +5,26 @@ describe User do
     describe "on a new user" do
       it "should not be valid with a blank password" do
         user = User.new(password: '')
-        user.save
+        user.valid?
         expect(user.errors[:password]).to match_array ["can't be blank"]
       end
 
       it "should not be valid with a confirmation mismatch" do
         user = User.new(password: 'alpha', password_confirmation: 'beta')
-        user.save
+        user.valid?
         expect(user.errors[:password_confirmation]).to match_array ["doesn't match Password"]
       end
 
       it "should not be valid without an email" do
         user = User.new(email: "")
-        user.save
+        user.valid?
         expect(user.errors[:email]).to match_array ["can't be blank"]
       end
 
       it "should not be valid with a currently registered email" do
         User.create!(email: "me@me.com", password: "iluvunicorns", password_confirmation: "iluvunicorns")
         user = User.new(email: "me@me.com")
-        user.save
+        user.valid?
         expect(user.errors[:email]).to match_array ["has already been taken"]
       end
     end
