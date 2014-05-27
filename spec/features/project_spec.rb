f7b81e10c6a69698353529bbdf0bb1eb830d08aa
Evan Tedesco <evantedesco@gmail.com>
2014-05-27 10:03:49 -0600

    [#71356942] Created project show page
    Signed-off-by: Bebe Peng <bebepeng@gmail.com>

--- spec/features/project_spec.rb
+++ spec/features/project_spec.rb
@@ -13,4 +13,14 @@ feature 'Project functions' do
     create_project
     expect(page).to have_content('Project name has already been taken')
   end
+
+  scenario 'Users can view a single project' do
+    create_project
+    click_on 'Murder Joffrey'
+    expect(page).to have_content 'Murder Joffrey'
+    within('table') do
+      expect(page).to have_content 'Name'
+      expect(page).to have_content 'Email'
+    end
+  end
 end
