4005b14f3ebbceeb9cf40b3ee60043495309d1ac
Gus King <gusssk@gmail.com>
2014-06-10 15:52:29 -0600

    updated request spec to submit content type as json
    Signed-off-by: Ellie Schneiders <ems2141@gmail.com>

--- spec/requests/api/events_spec.rb
+++ spec/requests/api/events_spec.rb
@@ -4,31 +4,33 @@ describe 'Events API' do
   it 'Will successfully create an event' do
     project = create_project
     api_key = project.project_api
-    post '/api/events', {
+    attributes = {
       occurred_on: 89765467,
       event_type: "User Registered",
       user_id: "user",
       properties: {
         sttingIn: "a"
       },
-      api_key: "#{api_key}"},
-         {'HTTP_ACCEPT' => "application/json"}
+      api_key: "#{api_key}"}
+    post '/api/events', JSON.generate(attributes), {'HTTP_ACCEPT' => "application/json", 'Content-Type' => 'application/json'}
 
-         expect(response).to be_success
+    expect(response).to be_success
 
   end
 
   it 'Will unsuccessfully create an event' do
     bad_api_key = 'jaodijfadsoijdsio8773'
-    post '/api/events', {
+    attributes = {
       occurred_on: 89765467,
       event_type: "User Registered",
       user_id: "user",
       properties: {
         sttingIn: "a"
       },
-      api_key: bad_api_key},
-         {'HTTP_ACCEPT' => "application/json"}
+      api_key: bad_api_key
+    }
+
+    post '/api/events', JSON.generate(attributes), {'HTTP_ACCEPT' => "application/json", 'Content-Type' => 'application/json'}
 
     expect(response).to_not be_success
 
